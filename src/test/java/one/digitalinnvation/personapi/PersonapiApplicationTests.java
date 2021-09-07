f6f2eba8f12dae8ad6f272a9d01aae1656e1f2f2
ygorrc <ygormagalhaesrc@gmail.com>
2021-09-07 19:06:00 -0300

    Adiciona configurações iniciais
    - Adicionado projeto Spring com suas dependências
    
    Dependências adicionadas:
    - Spring Web
    - Lombok
    - JPA
    - H2
    -Actuator
    - DevTools

---
+++ src/test/java/one/digitalinnvation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnvation.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
